7e9baccd3ac8a2341eb4c7eada78abbb72f0f9f8
Kehinde1980 <144909364+Kehinde1980@users.noreply.github.com>
2025-01-13 20:54:39 +0000

    Update app.js

--- videos-samples/5-s3-uploader/app.js
+++ videos-samples/5-s3-uploader/app.js
@@ -17,43 +17,41 @@
 // This is the function you will need in your Lambda to make the backend work.
 // Follow along in the tutorial to see how to set this up.
 
-const AWS = require('aws-sdk')
-AWS.config.update({ region: process.env.REGION })
-const s3 = new AWS.S3();
+import { S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 
-const uploadBucket = '<< ENTER YOUR BUCKET NAME HERE >>'   // << LOOK!
+const s3 = new S3Client({ region: process.env.REGION });
+const uploadBucket = 'liveproject4testing';
 
-exports.handler = async (event) => {
-  const result = await getUploadURL()
-  console.log('Result: ', result)
-  return result
+export const handler = async (event) => {
+  const result = await getUploadURL();
+  console.log('Result: ', result);
+  return result;
 };
 
-const getUploadURL = async function() {
-  console.log('getUploadURL started')
-  let actionId = Date.now()
+const getUploadURL = async () => {
+  console.log('getUploadURL started');
+  const actionId = Date.now();
 
-  var s3Params = {
+  const command = new PutObjectCommand({
     Bucket: uploadBucket,
-    Key:  `${actionId}.jpg`,
+    Key: `${actionId}.jpg`,
     ContentType: 'image/jpeg',
-//    CacheControl: 'max-age=31104000',
-//    ACL: 'public-read',   // Optional if you want the object to be publicly readable
-  };
+  });
 
-  return new Promise((resolve, reject) => {
-    // Get signed URL
-    let uploadURL = s3.getSignedUrl('putObject', s3Params)
-    resolve({
-      "statusCode": 200,
-      "isBase64Encoded": false,
-      "headers": {
-        "Access-Control-Allow-Origin": "*"
-      },
-      "body": JSON.stringify({
-          "uploadURL": uploadURL,
-          "photoFilename": `${actionId}.jpg`
-      })
-    })
-  })
-}
+  const uploadURL = await getSignedUrl(s3, command, { expiresIn: 3600 }); // URL expires in 1 hour
+
+  return {
+    statusCode: 200,
+    isBase64Encoded: false,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      uploadURL,
+      photoFilename: `${actionId}.jpg`,
+    }),
+  };
+};
+
